77889fa44dac5b0cff73f89858a4ca288d5e79cd
Roarke Catubig <roarkecatubig@gmail.com>
2018-06-14 13:56:27 -0700

    Changed server file with mongoose db code

--- server.js
+++ server.js
@@ -31,8 +31,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/week18Populater");
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+
+// Connect to the Mongo DB
+// mongoose.connect("mongodb://localhost/week18Populater");
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
